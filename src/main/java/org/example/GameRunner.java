a7ece55f43dc653190a17f8201dbf66ba5c50d40
dineshchintu <musuku.dinesh@practo.com>
2024-06-27 14:01:32 +0530

    files added

---
+++ src/main/java/org/example/GameRunner.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class GameRunner {
+    private final Game game;
+
+    public GameRunner(Game game) {
+        this.game = game;
+    }
+
+    public void run() {
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+}
---
+++ src/main/java/org/example/Pacman.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Pacman implements Game {
+    @Override
+    public void up() { System.out.println("Pacman moves up"); }
+    @Override
+    public void down() { System.out.println("Pacman moves down"); }
+    @Override
+    public void left() { System.out.println("Pacman moves left"); }
+    @Override
+    public void right() { System.out.println("Pacman moves right"); }
+}
